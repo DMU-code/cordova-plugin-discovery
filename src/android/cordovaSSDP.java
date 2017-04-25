4a031a53b4a21c69a2e7b3939e240685b96a32ad
Hans <Hikari@users.noreply.github.com>
2017-04-25 14:49:03 +0200

    Update cordovaSSDP.java
    removed the request to the XML doc for the android version

--- src/android/cordovaSSDP.java
+++ src/android/cordovaSSDP.java
@@ -128,7 +128,7 @@ public class cordovaSSDP extends CordovaPlugin {
                         device.put("LOCATION", parseHeaderValue(message, "LOCATION"));
                         device.put("ST", parseHeaderValue(message, "ST"));
                         device.put("Server", parseHeaderValue(message, "Server"));
-                        createServiceObjWithXMLData(parseHeaderValue(message, "LOCATION"), device);
+                        //createServiceObjWithXMLData(parseHeaderValue(message, "LOCATION"), device);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
